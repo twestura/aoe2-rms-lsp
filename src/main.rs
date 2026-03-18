d31c1f90d57151f7f5b4828fd5b4b3c2dd0cfed2
twestura <twestura@gmail.com>
2026-03-18 03:59:19 -0400

    Add initial lsp skeleton.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,50 @@
-fn main() {
-    println!("Hello, world!");
+//! Aoe2 RMS Language Server
+
+use tower_lsp::jsonrpc::Result;
+use tower_lsp::{
+    Client, LanguageServer, LspService, Server,
+    lsp_types::{InitializeParams, InitializeResult, InitializedParams, MessageType},
+};
+
+#[derive(Debug)]
+struct Backend {
+    client: Client,
+}
+
+#[tower_lsp::async_trait]
+impl LanguageServer for Backend {
+    async fn initialize(&self, _: InitializeParams) -> Result<InitializeResult> {
+        Ok(InitializeResult::default())
+    }
+
+    async fn initialized(&self, _: InitializedParams) {
+        self.client
+            .log_message(MessageType::INFO, "server initialized!")
+            .await;
+        log("server initialized!");
+    }
+
+    async fn shutdown(&self) -> Result<()> {
+        Ok(())
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let stdin = tokio::io::stdin();
+    let stdout = tokio::io::stdout();
+    let (service, socket) = LspService::new(|client| Backend { client });
+    Server::new(stdin, stdout, socket).serve(service).await;
+}
+
+/// Debugging function for logging messages from the LSP to a file.
+/// The file is ignored in the .gitignore file.
+fn log(msg: &str) {
+    use std::io::Write;
+    let mut file = std::fs::OpenOptions::new()
+        .create(true)
+        .append(true)
+        .open("aoe2-rms-lsp.txt")
+        .unwrap();
+    writeln!(file, "{msg}").unwrap();
 }
